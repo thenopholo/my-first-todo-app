f2d13e9e0544f90958a434a533359ce1c453d626
Rodrigo Thenopholo <thenopholo92@gmail.com>
2024-01-24 00:09:49 -0300

    Criação das primeiras funções do CRUD, o creare(save) e read(find, findAll)

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -11,8 +13,38 @@ class TaskDao {
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  save(Task taerfa) async{}
-  Future <List<Task>> findAll() async{}
-  Future <List<Task>>find(String nomeDaTarefa) async{}
-  delete(String nomeDaTarefa) async{}
+  save(Task taerfa) async {}
+  
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll');
+    final Database dataBase = await getDatabase();
+    final List<Map<String, dynamic>> result = await dataBase.query(_tablename);
+    print('Prucurando dados no banco de dados... Encontrado: $result');
+    return toList(result);
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('Convertendo to list: ');
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    print('lista de Tarefas $tarefas');
+    return tarefas;
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    print('Acessando o find: ');
+    final Database database = await getDatabase();
+    final List<Map<String, dynamic>> result = await database.query(
+      _tablename,
+      where: '$_name = ?',
+      whereArgs: [nomeDaTarefa],
+    );
+    print('Tarefa encontrada: ${toList(result)}');
+    return toList(result);
+  }
+
+  delete(String nomeDaTarefa) async {}
 }
